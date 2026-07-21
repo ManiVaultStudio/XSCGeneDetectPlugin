b994b4ecdf59121ae914f1c65d626b1e8a964a72
Soumyadeep Basu <44787782+sbvis@users.noreply.github.com>
2026-07-21 19:02:17 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class XSCGeneDetectPluginConan(ConanFile):
     description = """Viewer of cell XSCTreeData data as described in a .swc file."""
     topics = ("manivault", "plugin", "view", "XSCGeneDetectPlugin")
     url = "https://github.com/ManiVaultStudio/XSCGeneDetectPlugin"
-    author = "julianthijssen@gmail.com"  # conan recipe author
+    author = "sbasu"  # conan recipe author
     license = "LGPL 3.0"
 
     short_paths = True
